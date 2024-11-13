e40d1d950c319af04b3a232b154b6cf290e474fb
Billy <clementbilly04@gmail.com>
2024-11-13 01:13:20 +0100

    test

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const title = document.getElementById("title");
+    const homeBtn = document.getElementById("home_btn");
+
+    // Ajouter immédiatement la classe fade-in dès le chargement
+    title.classList.add("fade-in");
+    homeBtn.classList.add("fade-in");
+});
